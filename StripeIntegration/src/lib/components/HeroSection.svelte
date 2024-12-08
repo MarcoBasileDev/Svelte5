d6f6f959403981508aee54ef6813eeb93f04493a
MarcoBasileDev <marco.basile1996@gmail.com>
2024-12-08 11:02:09 +0100

    add AuthorSection component

--- StripeIntegration/src/lib/components/HeroSection.svelte
+++ StripeIntegration/src/lib/components/HeroSection.svelte
@@ -4,7 +4,7 @@
 	import phoneCover from "$assets/phone_cover.png";
 	import { Button } from "$components";
 
-	let { children } = $props();
+	//let { children } = $props();
 </script>
 
 <section class="hero">
@@ -18,9 +18,9 @@
 		<img class="phone-cover" src={phoneCover} alt="" />
 	</div>
 	<div class="hero-text white text-center">
-		{#if children}
+		<!-- {#if children}
 			{@render children()}
-		{:else}
+		{:else} -->
 			<h1>THIS EBOOK WILL</h1>
 			<h1>SAVE YOU MONEY,</h1>
 			<h1 class="mb-m">TIME & NERVES</h1>
@@ -31,7 +31,7 @@
 				this e-book, to tell you how I did it. No bullshit, no stress.
 			</p>
 			<Button>Purchase E-book for $10</Button>
-		{/if}
+		<!--{/if} -->
 	</div>
 </section>
 
