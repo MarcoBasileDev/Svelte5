139fe5fd65a0b7c983430e04a07c4afc26547c3a
MarcoBasileDev <marco.basile1996@gmail.com>
2024-12-11 00:07:25 +0100

    adjust hero section description

--- portfolio/src/lib/components/sections/HeroSection.svelte
+++ portfolio/src/lib/components/sections/HeroSection.svelte
@@ -10,7 +10,6 @@ import { goto } from '$app/navigation';
 <section class="hero-section default-margin pt-16">
 	<h3>Hello! I'm Marco</h3>
 	<div class="underscore mb-m"></div>
-	<h1 class="text-justify">I am a full - stack developer with a deep passion for the ever - evolving IT</h1>
-	<h1 class="dark-grey mb-m">landscape.</h1>
+	<h1 class="text-justify">I am a <span class="dark-grey">full-stack developer</span> with a passion for everything related to the world of IT.</h1>
 	<Button {onclick}>Let's talk</Button>
 </section>
