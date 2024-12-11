2abfe035fdedc74ea64eb974b9c43467b56fa770
MarcoBasileDev <marco.basile1996@gmail.com>
2024-12-11 21:01:46 +0100

    fix apikey

--- portfolio/src/routes/api/send-mail/+server.ts
+++ portfolio/src/routes/api/send-mail/+server.ts
@@ -1,27 +1,31 @@
-import {SENDGRID_API_KEY} from '$env/static/private';
-import sgMail from '@sendgrid/mail';
-import { json } from '@sveltejs/kit';
+import { SENDGRID_API_KEY } from "$env/static/private";
+import sgMail from "@sendgrid/mail";
+import { json } from "@sveltejs/kit";
 
 sgMail.setApiKey(SENDGRID_API_KEY);
 
-export async function POST({request}) {
-	const { contactName, contactEmail, projectInformation } = await request.json();
+export async function POST({ request }) {
+	const { contactMail, contactName, informationAboutProject } =
+		await request.json();
 
-	if (!contactName || !contactEmail || !projectInformation) {
-		return json({message: "Could not send email. Missing information."}, {status: 400});
+	if (!contactMail || !contactName || !informationAboutProject) {
+		json({ message: "Could not send email. Missing data." }, { status: 400 });
 	}
 
 	const message = {
-		to: 'marco.basile1996@gmail.com',
-		from: 'marco.basile1996@gmail.com',
-		subject: 'Contact Form on your portfolio',
-		html: `Somebody sent you an email\nName: ${contactName}\nEmail: ${contactEmail}\nProject Information: ${projectInformation}`
+		to: "niklas@kizo-agency.com",
+		from: "niklas@kizo-agency.com",
+		subject: "Contact Form on your portfolio",
+		html: `Somebody used the contact form on your site. <br/>
+    Name: ${contactName},
+    Email: ${contactMail},
+    Information about the project: ${informationAboutProject}`,
 	};
-	console.log(message);
+
 	try {
 		await sgMail.send(message);
-		return json({emailSentSuccessfully: true});
-	} catch (error) {
-		return json({error}, {status: 500});
+		return json({ emailSentSuccessfully: true });
+	} catch (err) {
+		return json({ err }, { status: 500 });
 	}
 }
