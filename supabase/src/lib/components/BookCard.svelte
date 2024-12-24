9fcd4ef833fe1c2b1e11bb3bb3af6518b9b03840
MarcoBasileDev <marco.basile1996@gmail.com>
2024-12-24 20:16:58 +0100

    finish BookCard.svelte

--- supabase/src/lib/components/BookCard.svelte
+++ supabase/src/lib/components/BookCard.svelte
@@ -5,11 +5,42 @@
 		book: Book;
 	}
 
+
 	let { book }: BookCardProps = $props();
+
+	/* posso fare un doppio ternario
+	let bookStatus = $derived(
+	book.finished_reading_on ? "Finished" : book.started_reading_on ? "Currently reading" : "Not started"
+); */
+
+	//oppure
+	let bookStatus = $derived.by(() => {
+		if (book.finished_reading_on) {
+			return "Finished";
+		}
+		if (book.started_reading_on) {
+			return "Currently reading";
+		}
+		return "Not started";
+	})
+
 </script>
 
 <a class="book-card" href={`/private/books/${book.id}`}>
-	<h4>{book.title}</h4>
+	<div class="book-status">
+		<span>{bookStatus}</span>
+	</div>
+	<div class="book-cover">
+		{#if book.cover_image}
+			<img src={book.cover_image} alt="" />
+		{/if}
+	</div>
+	<div class="book-info">
+		<h4>{book.title}</h4>
+		<p class="mb-s">{book.author}</p>
+		<p>Rating: {book.rating}</p>
+	</div>
+
 </a>
 
 
