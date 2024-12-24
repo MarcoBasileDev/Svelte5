76baf524040c1d3ca906a5ad0ca45cb41d1116ec
MarcoBasileDev <marco.basile1996@gmail.com>
2024-12-24 15:31:02 +0100

    add session handling

--- supabase/src/lib/state/user-state.svelte.ts
+++ supabase/src/lib/state/user-state.svelte.ts
@@ -1,4 +1,5 @@
 import { type Session, SupabaseClient, type User } from '@supabase/supabase-js';
+import { getContext, setContext } from 'svelte';
 
 interface UserStateProps {
 	session: Session | null;
@@ -6,7 +7,7 @@ interface UserStateProps {
 	user: User | null;
 }
 
-export class UserState {
+export class UserStateSvelte {
 	session = $state<Session | null>(null);
 	supabase = $state<SupabaseClient | null>(null);
 	user = $state<User | null>(null);
@@ -21,4 +22,18 @@ export class UserState {
 		this.user = data.user;
 	}
 
+	async logout() {
+		await this.supabase?.auth.signOut();
+	}
+
+}
+
+const USER_STATE_KEY = Symbol("USER_STATE");
+
+export function setUserState(data: UserStateProps) {
+	return setContext(USER_STATE_KEY, new UserStateSvelte(data));
+}
+
+export function getUserState() {
+	return getContext<ReturnType<typeof setUserState>>(USER_STATE_KEY);
 }
--- supabase/src/routes/+layout.svelte
+++ supabase/src/routes/+layout.svelte
@@ -3,9 +3,20 @@
 	import { Header } from '$components';
 	import { invalidate } from '$app/navigation'
 	import { onMount } from 'svelte'
+	import { setUserState } from '$lib/state/user-state.svelte';
 
 	let { data, children } = $props();
-	let { session, supabase } = $derived(data);
+	let { session, supabase, user } = $derived(data);
+
+	let userState = setUserState({
+		session: data.session,
+		supabase: data.supabase,
+		user: data.user
+	});
+
+	$effect(() => {
+		userState.updateState({session, supabase, user})
+	})
 
 	onMount(() => {
 		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
